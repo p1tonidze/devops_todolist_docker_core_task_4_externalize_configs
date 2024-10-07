11b3fc4eef0d2066f618da2dab7526c7b06e096c
p1tonidze <p1t0nidze422@gmail.com>
2024-10-07 11:48:25 +0300

    Solution

--- todolist/settings.py
+++ todolist/settings.py
@@ -10,6 +10,12 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+DB_ENGINE = os.environ.get("DB_ENGINE","")
+DB_NAME = os.environ.get("DB_NAME","")
+DB_USER = os.environ.get("DB_USER","")
+DB_PASSWORD = os.environ.get("DB_PASSWORD","")
+DB_HOST = os.environ.get("DB_HOST","")
+DB_PORT = os.environ.get("DB_PORT","")
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -62,12 +68,12 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'app_db',
-        'USER': 'app_user',
-        'PASSWORD': '1234',
-        'HOST': 'mysql',  # You can use a different host if your MySQL server is on a remote machine.
-        'PORT': '',  # Leave this empty to use the default MySQL port (3306).
+        'ENGINE': DB_ENGINE,
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,  # You can use a different host if your MySQL server is on a remote machine.
+        'PORT': DB_PORT,  # Leave this empty to use the default MySQL port (3306).
     }
 }
 
